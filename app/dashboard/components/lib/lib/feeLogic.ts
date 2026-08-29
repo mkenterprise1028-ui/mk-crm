6f5b722a4ac92ef119545218fd8847b65e4adf7c
mkenterprise1028-ui <mkenterprise1028@gmail.com>
2026-08-29 15:38:20 +0530

    Delete app directory

--- app/dashboard/components/lib/lib/feeLogic.ts
+++
@@ -1,6 +0,0 @@
-
-export function calculateTotalFee(configuration: string): number {
-  const FORM_FEE = configuration === 'Shop' ? 30500 : 22800;
-  const COMPULSORY = 300000;
-  return FORM_FEE + COMPULSORY;
-}
--- app/dashboard/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function DashboardPage() {
-  return (
-    <div style={{ padding: '24px' }}>
-      <h1>Dashboard Page</h1>
-      <p>Welcome to MK ENTERPRISES CRM Dashboard!</p>
-    </div>
-  )
-}
